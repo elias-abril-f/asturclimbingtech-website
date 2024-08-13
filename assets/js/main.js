750d97e9bfebd051c31f17d91a82a6e23f846b01
Elias Abril-Fernandez <50235043+elias-abril-f@users.noreply.github.com>
2024-08-13 11:27:17 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -347,31 +347,31 @@
          * Youtube video background
          /* ---------------------------------------------- */
 
-        $(function(){
-            $(".video-player").mb_YTPlayer();
-        });
+        // $(function(){
+        //     $(".video-player").mb_YTPlayer();
+        // });
 
-        $('#video-play').click(function(event) {
-            event.preventDefault();
-            if ($(this).hasClass('fa-play')) {
-                $('.video-player').playYTP();
-            } else {
-                $('.video-player').pauseYTP();
-            }
-            $(this).toggleClass('fa-play fa-pause');
-            return false;
-        });
+        // $('#video-play').click(function(event) {
+        //     event.preventDefault();
+        //     if ($(this).hasClass('fa-play')) {
+        //         $('.video-player').playYTP();
+        //     } else {
+        //         $('.video-player').pauseYTP();
+        //     }
+        //     $(this).toggleClass('fa-play fa-pause');
+        //     return false;
+        // });
 
-        $('#video-volume').click(function(event) {
-            event.preventDefault();
-            if ($(this).hasClass('fa-volume-off')) {
-                $('.video-player').YTPUnmute();
-            } else {
-                $('.video-player').YTPMute();
-            }
-            $(this).toggleClass('fa-volume-off fa-volume-up');
-            return false;
-        });
+        // $('#video-volume').click(function(event) {
+        //     event.preventDefault();
+        //     if ($(this).hasClass('fa-volume-off')) {
+        //         $('.video-player').YTPUnmute();
+        //     } else {
+        //         $('.video-player').YTPMute();
+        //     }
+        //     $(this).toggleClass('fa-volume-off fa-volume-up');
+        //     return false;
+        // });
 
 
         /* ---------------------------------------------- /*

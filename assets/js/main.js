d9de7b9fbfb8164d0afda93344d00c4be0277f33
Elias <elias.abril.f@gmail.com>
2025-08-26 14:02:28 +0100

    Fixed error occurd contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -444,33 +444,33 @@
         $("#contactForm").submit(function (e) {
 
             e.preventDefault();
-            var $ = jQuery;
+            // var $ = jQuery;
 
-            var postData = $(this).serializeArray(),
-                formURL = $(this).attr("action"),
-                $cfResponse = $('#contactFormResponse'),
-                $cfsubmit = $("#cfsubmit"),
-                cfsubmitText = $cfsubmit.text();
+            // var postData = $(this).serializeArray(),
+            //     formURL = $(this).attr("action"),
+            //     $cfResponse = $('#contactFormResponse'),
+            //     $cfsubmit = $("#cfsubmit"),
+            //     cfsubmitText = $cfsubmit.text();
 
-            $cfsubmit.text("Sending...");
+            // $cfsubmit.text("Sending...");
 
 
-            $.ajax(
-                {
-                    url: formURL,
-                    type: "POST",
-                    data: postData,
-                    success: function (data) {
-                        $cfResponse.html(data);
-                        $cfsubmit.text(cfsubmitText);
-                        $('#contactForm input[name=name]').val('');
-                        $('#contactForm input[name=email]').val('');
-                        $('#contactForm textarea[name=message]').val('');
-                    },
-                    error: function (data) {
-                        alert("Error occurd! Please try again");
-                    }
-                });
+            // $.ajax(
+            //     {
+            //         url: formURL,
+            //         type: "POST",
+            //         data: postData,
+            //         success: function (data) {
+            //             $cfResponse.html(data);
+            //             $cfsubmit.text(cfsubmitText);
+            //             $('#contactForm input[name=name]').val('');
+            //             $('#contactForm input[name=email]').val('');
+            //             $('#contactForm textarea[name=message]').val('');
+            //         },
+            //         error: function (data) {
+            //             alert("Error occurd! Please try again");
+            //         }
+            //     });
 
             return false;
 
